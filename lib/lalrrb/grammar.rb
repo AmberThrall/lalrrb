3b1cb9e2120a996d88f9a40cbb5c4ca688f5ed7a
Amber Thrall <amber.rose.thrall@gmail.com>
2021-09-01 13:04:23 -0700

    Grammar rules and tokens no longer raise NameError issues

--- lib/lalrrb/grammar.rb
+++ lib/lalrrb/grammar.rb
@@ -14,7 +14,7 @@ module Lalrrb
 
     def self.tokens
       # Convert terminals into tokens
-      terminals = @rules.map { |_, rule| rule.search(:terminal, expand: true) }.flatten
+      terminals = @rules.nil? ? [] : @rules.map { |_, rule| rule.search(:terminal, expand: true) }.flatten
       terminals.each do |t|
         lexer.token(t.match, t.match)
       end
@@ -34,12 +34,26 @@ module Lalrrb
 
     def self.token(name, match, *flags, &block)
       name = name.to_sym
+      @rules ||= {}
+      return lexer.tokens[name] if lexer.tokens.include?(name) && match.nil? && flags.empty? && block.nil?
 
-      raise Error, "Token '#{name}' should be all capitalized." unless name.to_s.upcase == name.to_s
-      raise Error, "Token '#{name}' already defined." if const_defined?(name)
+      raise Error, "Token #{name} already defined." if lexer.tokens.include?(name)
+      raise Error, "Token #{name} conflicts with rule #{name}." if @rules.include?(name)
+      raise Error, "Method #{name}? already defined." if method_defined?("#{name}?".to_sym)
 
-      lexer.token(name, match, *flags, &block)
-      const_set(name, Terminal.new(match, name: name))
+      begin
+        raise Error, "Constant #{name} already defined." if const_defined?(name)
+
+        lexer.token(name, match, *flags, &block)
+        const_set(name, Terminal.new(match, name: name))
+      rescue NameError
+        raise Error, "Method #{name} already defined." if method_defined?(name)
+
+        lexer.token(name, match, *flags, &block)
+        define_method(name) { Terminal.new(match, name: name) }
+      end
+
+      define_method("#{name}?".to_sym) { Optional.new(Terminal.new(match, name: name)) }
     end
 
     def self.ignore(match, *flags)
@@ -48,13 +62,27 @@ module Lalrrb
 
     def self.rule(name, &block)
       name = name.to_sym
-      raise Error, "Rule '#{name}' already defined." if method_defined?(name) || method_defined?("#{name}?".to_sym)
-
       @rules ||= {}
-      block_closure = proc { instance.instance_eval(&block) }
-      @rules[name] = Rule.new(name, &block_closure)
+      return @rules[name] if @rules.include?(name) && block.nil?
 
-      define_method(name) { self.class.rules[name] }
+      raise Error, "Rule #{name} already defined." if @rules.include?(name)
+      raise Error, "Rule #{name} conflicts with token #{name}." if lexer.tokens.include?(name)
+      raise Error, "Method #{name}? already defined." if method_defined?("#{name}?".to_sym)
+
+      block_closure = proc { instance.instance_eval(&block) }
+
+      begin
+        raise Error, "Constant #{name} already defined." if const_defined?(name)
+
+        @rules[name] = Rule.new(name, &block_closure)
+        const_set(name, rules[name])
+      rescue NameError
+        raise Error, "Method #{name} already defined." if method_defined?(name)
+
+        @rules[name] = Rule.new(name, &block_closure)
+        define_method(name) { self.class.rules[name] }
+      end
+
       define_method("#{name}?".to_sym) { self.class.rules[name].optional }
     end
 
